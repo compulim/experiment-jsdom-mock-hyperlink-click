72e57203f0139af92d6113499debb98c9f6f8441
William Wong <compulim@hotmail.com>
2022-09-07 11:46:27 -0700

    Clean up

--- src/MyComponent.spec.jsx
+++ src/MyComponent.spec.jsx
@@ -1,9 +1,9 @@
 /* @jest-environment jsdom */
 
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
 
 function mockCreateElement(onClick) {
   return {
